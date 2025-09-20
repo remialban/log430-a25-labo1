809e27cf8d9262f1c64454189a7d2baf5a9e4df0
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-09-20 02:51:59 +0200

    update

--- src/store_manager.py
+++ src/store_manager.py
@@ -4,8 +4,11 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 from views.user_view import UserView
+from views.product_view import ProductView
 
 if __name__ == '__main__':
     print("===== LE MAGASIN DU COIN =====")
-    main_menu = UserView()
-    main_menu.show_options()
+    #main_menu = UserView()
+    #main_menu.show_options()
+    product_menu = ProductView()
+    product_menu.show_options()
